490a5650e6ff6c2763a0ac04be092dffb3cd0899
cpx <1004240239@qq.com>
2024-06-20 22:27:33 +0800

    feat: prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,24 @@
 module.exports = {
-  extends: ["ali/typescript/react", "plugin:prettier/recommended"],
+  extends: ['ali/typescript/react', 'plugin:prettier/recommended'],
   // plugins: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
   rules: {
     // quotes: ["error", "double"],
     // prettier与eslint规则冲突，使用prettier作为覆盖规则
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        singleQuote: false,
+        // singleQuote: false,
       },
     ],
   },
---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  plugins: ['prettier-config-ali'],
+  // singleQuote: true,
+};
